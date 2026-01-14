2699d228acaa69741a3fe4129c05c3e6a7fba8be
Christoph Martin <cmarti@evolit.co.at>
2026-01-14 13:03:30 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/endure-website',
+  site: 'https://www.endure-cycling.com',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
